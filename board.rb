d89c2da40471f2ab29f0c709088fa892782f6010
App Academy Student <student@appacademy.io>
2015-10-30 13:18:32 -0700

    figuring out surrounding moves

--- board.rb
+++ board.rb
@@ -1,2 +1,20 @@
 class Board
+  def initialize(length = 9, width = 9)
+    @grid = Array.new(width) {Array.new(length)}
+    @moves = all_moves
+  end
+
+  def all_moves
+    arr = []
+    i = 0
+    while i < @grid.length
+      j = 0
+      while j < @grid[0].length
+        arr < [i, j]
+        j += 1
+      end
+      i += 1
+    end
+    arr
+  end
 end
--- tile.rb
+++ tile.rb
@@ -1,2 +1,46 @@
+require './board.rb'
+
 class Tile
+
+  def initialize(board, pos)
+    @pos = pos
+    @board = board
+    @flagged = false
+    @bombed = false
+    @revealed = false
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+  def find_surrounding_tiles(@grid, x, y)
+    8.times do |num|
+      if board.grid(x-1, y).is_a?(Boolean)
+
+    end
+
+  end
+
+  def neighbors
+  end
+
+  def neighbor_bomb_count
+  end
+
+  def inspect
+
+  end
+
+  private
+  MOVES = [
+    [ 1, 0]
+    [-1, 0]
+    [ 0, 1]
+    [ 0,-1]
+    [ 1, 1]
+    [-1,-1]
+    [-1, 1]
+    [1, -1]
+  ]
 end
